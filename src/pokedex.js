21628594cf524f1bf5793dff78577af93d80e622
marinabrcls <barcelos.marina@gmail.com>
2020-07-26 23:19:19 -0300

    last changes before deleting branch

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,22 +1,60 @@
-/*
-1. esse componente representa a enciclopédia de pokémons;
-2. esse componente recebe como entrada uma lista de pokémons
-para serem mostrados na tela;
-3. para cada pokémon recebido, Pokedex chama o componente Pokemon.
-*/
-
 import React from 'react';
-import Pokemon from './pokemon';
-import pokemons from './data';
+import pokemonList from './data';
+import Pokemons from './pokemon'
 
-class Pokedex extends React.Component {
+class pokedex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemonIndex: 0,
+      filterType: 'Fire'
+    }
+  }
+
+  handleClick = (listLength) => {
+    this.nextPokemon(this.state, listLength);
+  }
+
+  nextPokemon = (state, listLength) => {
+    this.setState({
+      pokemonIndex: state.pokemonIndex + 1 >= listLength ? 0 : state.pokemonIndex + 1
+    })
+  }
+
+  setFilter = (state, filter) => {
+    this.setState({
+      pokemonIndex: state.pokemonIndex,
+      filterType: filter
+    })
+  }
+
   render() {
+    const filteredPokemons = pokemonList
+      .filter((pokemon) => this.state.filterType === pokemon.type)
+
+    const { name, type, image, averageWeight } = filteredPokemons[this.state.pokemonIndex];
+
     return (
-      <div className="PokemonDiv">
-        {pokemons.map(pokemon => <Pokemon pokemon={pokemon} />)}
+      <div>
+        <Pokemons
+          name={name}
+          type={type}
+          image={image}
+          weight={averageWeight.value}
+          unit={averageWeight.measurementUnit}
+        />
+        <button onClick={() => this.handleClick(filteredPokemons.length)}>Proximo</button>
+        <button onClick={() => this.setFilter(this.state, 'Bug')}>Bug</button>
+        <button onClick={() => this.setFilter(this.state, 'Dragon')}>Dragon</button>
+        <button onClick={() => this.setFilter(this.state, 'Electric')}>Electric</button>
+        <button onClick={() => this.setFilter(this.state, 'Fire')}>Fire</button>
+        <button onClick={() => this.setFilter(this.state, 'Normal')}>Normal</button>
+        <button onClick={() => this.setFilter(this.state, 'Poison')}>Poison</button>
+        <button onClick={() => this.setFilter(this.state, 'Psychic')}>Psychic</button>
+
       </div>
     );
   }
 }
 
-export default Pokedex;
+export default pokedex;
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,30 +1,18 @@
-/* 
-1. esse componente recebe como entrada um objeto que contém
-informações referentes a um pokémon específico;
-2. esse componente precisa retornar as seguintes informações:
-- nome do pokémon;
-- tipo do pokémon;
-- peso médio do pokémon, acompanhado da unidade de medida;
-- imagem do pokémon;
-*/
-
 import React from 'react';
 
-class Pokemon extends React.Component {
+class pokemon extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <div className='container-info'>
-          <p> {this.props.pokemon.name} </p>
-          <p> {this.props.pokemon.type} </p>
-          <p> {this.props.pokemon.averageWeight.value} {this.props.pokemon.averageWeight.measurementUnit} </p>
-        </div>
+      <section className={`pokecontainer ${this.props.type}`}>
         <div>
-          <img src ={this.props.pokemon.image} />
+          <p>{this.props.name}</p>
+          <p>{this.props.type}</p>
+          <p>{`${this.props.weight} ${this.props.unit}`}</p>
         </div>
-      </div>
-    )
+        <img src={this.props.image} />
+      </section>
+    );
   }
 }
 
-export default Pokemon;
+export default pokemon;
